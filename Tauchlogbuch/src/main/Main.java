dde4dced9743f7415ee42752bf9f821b205e6629
Chrioph <christophhegerath@gmx.de>
2018-12-17 19:02:26 +0100

    Implemented MenuController

---
+++ Tauchlogbuch/src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import view.MenuController;
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Start");
+        MenuController menu = new MenuController();
+
+    }
+}
